ae778798983ada0732edd6d82840679bb55f8979
Yusuke Watanabe <yusukew052@gmail.com>
2015-08-22 00:04:43 +0900

    update to use gulp-pleeease

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
+var pleeease = require("gulp-pleeease");
+
 gulp.task('sass', function() {
   return sass('src/', { style: 'expanded' })
     .on('error', function (err) {
       console.error('Error!', err.message);
     })
+    .pipe(pleeease({
+      minifier: false
+    }))
     .pipe(gulp.dest('./build'));
 });
 
