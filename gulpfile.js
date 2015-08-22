ead4fb2da1c94689c8baada8868b6ba0f11aee4c
Yusuke Watanabe <yusukew052@gmail.com>
2015-08-23 00:35:04 +0900

    support minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 var pleeease = require("gulp-pleeease");
+var rename = require("gulp-rename");
 
 gulp.task('sass', function() {
   return sass('src/', { style: 'expanded' })
@@ -13,4 +14,18 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('./build'));
 });
 
-gulp.task('default', ['sass']);
+gulp.task('minify', function() {
+  return sass('src/', { style: 'expanded' })
+    .on('error', function (err) {
+      console.error('Error!', err.message);
+    })
+    .pipe(pleeease({
+      minifier: true
+    }))
+    .pipe(rename({
+      extname: '.min.css'
+    }))
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('default', ['sass', 'minify']);
